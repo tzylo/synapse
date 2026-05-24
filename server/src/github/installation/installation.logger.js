7b824547eed4bb2bbf5cffb8f7baaa123e3bf125
K B Pramod <kbpramod3@gmail.com>
2026-05-24 19:34:58 +0530

    fix: moved installation logs to server folder from src

--- server/src/github/installation/installation.logger.js
+++ server/src/github/installation/installation.logger.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const logDir = path.join(__dirname, "../../logs");
+const logDir = path.join(__dirname, "../../../logs");
 
 if (!fs.existsSync(logDir)) {
     fs.mkdirSync(logDir, { recursive: true });
